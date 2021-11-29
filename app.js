6ffcc937a3f914cac5675155258b7dc6f1458d83
122zion <56268884+122zion@users.noreply.github.com>
2021-11-29 13:56:01 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-Array.from(titles).forEach(function(title){
-  console.log(title);
-});
-
-Array.from(titles).forEach(title => console.log(title));
+const books = list.getElementsByTagName('li');
+Array.from(books).forEach((book) => {
+  const title = book.firstElementChild.textContent;
+  if(title.toLowerCase().indexOf(e.target.value) != -1){
+  if(title.toLowerCase().indexOf(term) != -1){
+    book.style.display = 'block';
+  } else {
+    book.style.display = 'none';
